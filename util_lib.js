b75ed4937f00fd4ac7052477aa74cea36f575ead
Lysander Adriel Gawaran <glysander22@gmail.com>
2023-02-01 04:50:35 +0800

    Update util_lib.js
    added unique function

--- util_lib.js
+++ util_lib.js
@@ -1,35 +1,44 @@
-define(["N/query", "N/record", "N/search"], function (query, record, search) {
-    
+define(["N/query", "N/record", "N/search"], function(query, record, search) {
+
     function isEmpty(stValue) {
-            return ((stValue === '' || stValue == null || stValue == undefined) || (stValue.constructor === Array && stValue.length == 0) || (stValue.constructor === Object && (function (v) {
-                for (var k in v)
-                    return false;
-                return true;
-            })(stValue)));
-     }
-    
+        return ((stValue === '' || stValue == null || stValue == undefined) || (stValue.constructor === Array && stValue.length == 0) || (stValue.constructor === Object && (function(v) {
+            for (var k in v)
+                return false;
+            return true;
+        })(stValue)));
+    }
+
     function getKeyByValue(object, value) {
         return Object.keys(object).find(key => object[key] == value);
     }
-    
-    function getFullResultset (objSearch) {
-		let searchResults = [],
-			pagedData;
-		let searchCols = objSearch.columns;
-		
-		pagedData = objSearch.runPaged({
-			pageSize: 1000
-		});
-		pagedData.pageRanges.forEach(function(pageRange) {
-			let page = pagedData.fetch({
-				index: pageRange.index
-			});
-			page.data.forEach(
-				function(result) {
-					searchResults.push(result);
-			});
-		})
+
+    function getFullResultset(objSearch) {
+        let searchResults = [],
+            pagedData;
+        let searchCols = objSearch.columns;
+
+        pagedData = objSearch.runPaged({
+            pageSize: 1000
+        });
+        pagedData.pageRanges.forEach(function(pageRange) {
+            let page = pagedData.fetch({
+                index: pageRange.index
+            });
+            page.data.forEach(
+                function(result) {
+                    searchResults.push(result);
+                });
+        })
     }
-        
-    
+
+    function unique(array) {
+        return array.reduce(function(result, value) {
+            if (result.indexOf(value) === -1) {
+                result.push(value);
+            }
+            return result;
+        }, []);
+    }
+
+
 });
